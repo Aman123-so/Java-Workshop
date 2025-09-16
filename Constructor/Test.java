0b630950f576dd12c459f6b32a8584b78bf87582
Aman Gupta <https://github.com/Aman123-so/Aman-demo-repo.git>
2025-09-16 14:36:04 +0530

    first commit

--- Constructor/Test.java
+++ Constructor/Test.java
@@ -6,13 +6,17 @@ class Test {
 
     }
     Test(int age){
-        System.out.println(age);
+        System.out.println("Age:" +age);
+    }
+    Test(double  Salary){
+          System.out.println( "Salary : " +Salary);
     }
 
     public static void main(String[] args) {
 
         Test obj1 = new Test("Aman");
         Test obj2 = new Test(18);
+        Test obj3 = new Test(35000.50);
 
     }
 }
